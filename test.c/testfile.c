b5a92eb0c9a9a938d067e655fc28509e368b51b0
MagadiM <milliemagadi4@gmail.com>
2022-08-09 17:18:10 -0700

    push all

---
+++ test.c/testfile.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main()
+{
+	int n = printf("Sunshine");
+	printf("n = %d \n", n);
+	printf("Sunshine %c", A);
+	printf("Sunshine %s", meeting);
+	printf("sunshine %%", %);
+
+	return (0);
+}
